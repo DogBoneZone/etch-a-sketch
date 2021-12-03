32b6596d42bbc67e0a79d65fcbdd15d19bb20db1
dogbonezone <alexrhern@gmail.com>
2021-12-02 18:06:59 -0800

    Created grid

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 // Create square grid
-let body = document.querySelector('body');
-let container = document.createElement("div");
+const container = document.createElement("div");
 container.setAttribute("id", "container");
-body.appendChild(container);
+document.body.appendChild(container);
 
 // Create individual squares for grid
-const square = document.createElement("div");
-square.setAttribute("class", "square");
-
 let gridSize = 16;
 
 for (let i=1; i <= gridSize; i++) {
+    const square = document.createElement("div");
+    square.setAttribute("class", "square");
+    square.style.background = "white";
     container.appendChild(square);
 }
