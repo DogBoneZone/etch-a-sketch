189fee4c9fc5dd7a56118cd4f2763180b0908059
dogbonezone <alexrhern@gmail.com>
2021-12-07 21:35:56 -0800

    Completed logic and basic formatting

--- script.js
+++ script.js
@@ -1,65 +1,92 @@
 // Global Variables
 let gridSize = 16;
 let paintColor = '#000000';
+const paintZone = document.querySelector("#paintZone");
 const colorPicker = document.querySelector("#colorPicker");
 const randomColor = document.querySelector("#random");
+const rainbowColor = document.querySelector("#rainbow");
 const clearAllButton = document.querySelector("#clearAll");
+const header = document.querySelector("header");
+let colorMode;
+
+// Create Grid Size Label
+let gridSizeLabel = document.createElement("div");
+gridSizeLabel.setAttribute("id", "gridSizeLabel");
+gridSizeLabel.style.fontSize = "large";
+gridSizeLabel.textContent = `${gridSize} x ${gridSize}`;
+header.appendChild(gridSizeLabel);
 
 // Painting Function
 const paintFunc = () => {
     document.querySelectorAll(".square").forEach(item => {
     item.addEventListener("mouseover", () => {
+        if (colorMode === "rainbow") {
+            randomColorFunc();
+        }
         item.style.backgroundColor = paintColor;
         })
     })
 };
 
-// Create grid
+// Create grid squares
 const createGrid = (size) => {
-    console.log(gridSize);
-    for (let i=1; i <= (gridSize*gridSize); i++) {
+    gridDiv.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    gridDiv.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+    for (let i=1; i <= (size*size); i++) {
         const square = document.createElement("div");
         square.setAttribute("class", "square");
-        square.style.height = `${(600/gridSize)}px`;
-        square.style.width = `${(600/gridSize)}px`;
         square.style.background = "white";
-        container.appendChild(square);
-
-        paintFunc();
+        grid.appendChild(square);
     }
+        updateGridSizeLabel(gridSize);
+        paintFunc();
 };
 
-// Create squares in grid
-const container = document.createElement("div");
-container.setAttribute("id", "container");
-document.body.appendChild(container);
-const containerDiv = document.querySelector("#container");
+// Create grid grid
+const grid = document.createElement("div");
+grid.setAttribute("id", "grid");
+paintZone.appendChild(grid);
+const gridDiv = document.querySelector("#grid");
 createGrid(gridSize);
 
 const removeChildNodes = (parent) => {
-    containerDiv.innerHTML = '';
+    parent.innerHTML = '';
 };
 
 // Clear All Button
 clearAllButton.addEventListener("click", () => {
-    let gridInquiry = prompt("Select a grid size up to 100");
-    if (gridInquiry > 100) {
-        window.alert("Please enter a number from 1-100");
+    let gridInquiry = prompt("Select a grid size up to 64");
+    if (gridInquiry > 64||gridInquiry < 1) {
+        window.alert("Please enter a number from 1-64");
     } else {
         gridSize = Number(gridInquiry);
     }
 
-    removeChildNodes(containerDiv);
+    removeChildNodes(gridDiv);
     
     createGrid(gridSize);
 });
 
-// Paint Color Changer
+// Paint Color Changers
 colorPicker.addEventListener("change", (event) => {
+    colorMode = "";
     paintColor = event.target.value;
 });
 
 randomColor.addEventListener("click", () => {
-    let randomRGB = Math.random() * 360;
-    paintColor = `hsl(${randomRGB}, 100%, 50%)`;
+    colorMode = "";
+    randomColorFunc();
 });
+
+rainbowColor.addEventListener("click", () => {
+    colorMode = "rainbow";
+});
+
+function randomColorFunc() {
+    paintColor = `hsl(${Math.random() * 360}, ${Math.random() * 100}%, 50%)`;
+};
+
+function updateGridSizeLabel(newSize) {
+    gridSizeLabel.textContent = `${newSize} x ${newSize}`;
+}
